be6ad16bf90fd5421a9c89db6959cb1f42d8e6b0
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-03-31 17:11:24 -0300

    FEAT configurando estrutura da aplicação

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './styles/components/Button'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/themes/global'
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secundary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
