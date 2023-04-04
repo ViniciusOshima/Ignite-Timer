de55737fa1ed7ccc827eb049329acd7a0e489335
ViniciusOshima <vinicius_hideaki@outlook.com>
2023-04-03 21:36:56 -0300

    FEAT introdução a estilização da page Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,39 @@
+import {
+  HomeContainer,
+  FormContainer,
+  CountDownContainer,
+  Separator
+} from './styles'
+
+import { Play } from 'phosphor-react'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="number" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </HomeContainer>
+  )
+}
